c1fd6c2a59519f78c07d65a3c1a77556c0fbc634
Christopher Scott <christopher@christopherscott.ca>
2022-01-15 23:06:32 -0800

    Supporting pickline of `AO3.Work objects`.
    Really, this adds support for pickling of BeautifulSoup objects,
    which each AO3.Work includes as an attribute.

--- test/ao3_work.py
+++ test/ao3_work.py
@@ -1,10 +1,11 @@
 """ Provides an `AO3.Work` for testing. """
 
+import sys
 import warnings
 import AO3
 import pickle
 
-PICKLE_FILE = 'work_pickle'
+PICKLE_FILE = 'test/work_pickle'
 TEST_WORK_ID = 18117086
 
 # To limit network use during tests, try to load the Work via pickle:
@@ -16,9 +17,17 @@ except:
     TEST_WORK = AO3.Work(TEST_WORK_ID)
     # Attempt to write this to file to accelerate future tests:
     try:
-        with open(PICKLE_FILE, 'wb'):
+        # Increase recursion limit to account for high level of
+        # recursion in BeautifulSoup objects. See:
+        # https://docs.python.org/3/library/pickle.html#what-can-be-pickled-and-unpickled
+        # https://stackoverflow.com/a/52975220
+        limit = sys.getrecursionlimit()
+        sys.setrecursionlimit(10000)
+        with open(PICKLE_FILE, 'wb') as file:
             pickle.dump(TEST_WORK, file, pickle.HIGHEST_PROTOCOL)
     except:
         warnings.warn(
             "Could not write test work to disk. " +
             "Repeated tests may result in rate-limiting.")
+    finally:
+        sys.setrecursionlimit(limit)
