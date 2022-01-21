a565ca5c635bf7c6a4fa25a333536a35a0638c4c
Christopher Scott <christopher@christopherscott.ca>
2022-01-21 14:40:30 -0800

    Basic implementation of `/feed` endpoint.
    This serves an OPDS feed of a user's Marked for Later list
    at the `/feed` endpoint. It can be called by a logged-in user,
    or by an anonymous client if they
    pass the user's AO3 credentials as GET parameters.
    (These are not stored by the app, although the server software
    may log these parameters. It is not very safe!)
    
    Next steps: Providing each feed with a secret sharable address.

--- ao3opds/app/feed.py
+++ ao3opds/app/feed.py
@@ -1,35 +1,80 @@
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for)
+import datetime
+from flask import Blueprint, g, request, url_for
 from werkzeug.exceptions import abort
-from ao3opds.app.auth import login_required
 from ao3opds.app.db import get_db
-from ao3opds.marked_for_later_opds import get_marked_for_later_opds
+from ao3opds.app.ao3 import REFRESH_FREQUENCY, ao3_session_required
+from ao3opds.marked_for_later_opds import get_marked_for_later_opds, FEED_AUTHOR
+import AO3
+
+# Feeds are stale after 5 minutes:
+REFRESH_FREQUENCY = datetime.timedelta(minutes=5)
+FEED_TYPE_MARKED_FOR_LATER = "marked_for_later"
 
 blueprint = Blueprint('feed', __name__, url_prefix='/feed')
 
-@blueprint.route('/')
-# User must be logged in to do this
-# TODO: Consider whether instead we should return HTTP status 401
-# [Unauthorized] if accessing a feed endpoint rather than redirect to
-# `login` (as `login_required` does), as the client is likely to be
-# an ebook reader rather than a client. (Consider whether it is possible
-# to issue a 401 error and also a redirect...)
-@login_required
+def render_marked_for_later_feed(session: AO3.Session):
+    """ Generates an OPDS feed for a user's Marked for Later list """
+    feed_id = url_for('feed.marked_for_later')
+    feed_title = f"{session.username}'s Marked for Later list"
+    feed = get_marked_for_later_opds(
+        session, feed_id, feed_title, [FEED_AUTHOR])
+    return feed
+
+# We support a few modes here; a user can be logged in, or they can
+# provide their AO3 credentials (as `u` and `p` GET parameters) without
+# having an account:
+@blueprint.route('/', methods=['GET'])
 def marked_for_later():
     """ An OPDS v. 1.2 feed of a user's AO3 Marked for Later works. """
-    # TODO: Acquire user credentials
+    # Support anonymous mode:
+    if (
+            g.ao3_session is None and  # User not logged in
+            request.args.get['u'] and request.args.get['p'] # Credentials provided
+        ):
+        # Spin up an anonymous session:
+        ao3_username = request.args.get['u']
+        ao3_password = request.args.get['p']
+        try:
+            session = AO3.Session(ao3_username, ao3_password)
+        except AO3.utils.LoginError as err:
+            abort(401, "Could not authenticate with AO3: " + str(err))
+        return render_marked_for_later_feed(session)
+    # Check to see whether there is an active AO3 session:
+    elif g.ao3_session is None:  # no logged in user, no credentials:
+        abort(401, "Must be logged in to see Marked for Later feed.")
 
-    # TODO: Check user.updated, return cached feed if recent
+    # Ok, if we get here then we must be logged in.
+    # Acquire user credentials and other useful data:
+    user_id = g.user['id']
+    ao3_id = g.ao3['id']
+    session = g.ao3_session
+    db = get_db()
 
-    # TODO: Otherwise, get AO3 credentials.
-    # NOTE: We'll need to store AO3 credentials in order to do this.
-    # There is no truly safe way to do this, but look into mitigation
-    # techniques to improve security.
+    # Get the cached feed, if it exists:
+    feed = db.execute(
+        'SELECT * FROM feed WHERE (user_id = ? AND feed_type = ?)',
+        (user_id, FEED_TYPE_MARKED_FOR_LATER)
+    ).fetchone()
 
-    # TODO: Pass AO3 credentials to `get_marked_for_later_opds` to
-    # attempt loading from AO3. Check for errors/None return value
+    # Generate a new feed if there's no cached feed (of if it's old)
+    if (
+            feed is None or
+            feed['updated'] < datetime.datetime.now() - REFRESH_FREQUENCY
+        ):
+        # Refresh the feed:
+        new_feed = render_marked_for_later_feed(session)
+        # Store the feed:
+        if feed is None:  # insert new feed if no existing feed
+            db.execute(
+                'INSERT INTO feed (user_id, ao3_id, feed_type, content)'
+                ' VALUES (?, ?, ?, ?)',
+                (user_id, ao3_id, FEED_TYPE_MARKED_FOR_LATER, new_feed))
+        else:  # update existing feed if it exists
+            db.execute(
+                'UPDATE feed SET (content = ?, updated = CURRENT_TIMESTAMP)'
+                ' WHERE id = ?', (new_feed, feed['id']))
+        # Replace the old feed with the new feed, to show to the user:
+        feed = new_feed
 
-    # TODO: Handle error case (`flash`+redirect, HTTP error code?)
-
-    # TODO: Return OPDS feed
-    pass
+    # No need to render; `feed` is already a rendered template:
+    return feed
