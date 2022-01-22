b01c68eb078d764a4d3b913f6115969992c4b421
Christopher Scott <christopher@christopherscott.ca>
2022-01-22 00:36:17 -0800

    Adds bookmarks, subscriptions, and history feeds.
    This commit adds a generic `_feed_opds` and several
    functions for specific feeds that call it.
    The result is a module that's not quite twice as long but provides
    four times as many feeds!

--- ao3opds/render.py
+++ ao3opds/render.py
@@ -5,44 +5,33 @@ import AO3
 from ao3opds.opds import OPDSPerson, AO3OPDS
 
 # Default values for Feed:
-FEED_ID = "christopherscott.ca/apps/ao3opds/{username}"
-FEED_TITLE = "{username}'s AO3 Marked for Later list"
+FEED_NAMES = {
+    'marked_for_later': "Marked for Later",
+    'bookmarks': "Bookmarks",
+    'subscriptions': "Subscriptions",
+    'history': "History"
+}
+FEED_ID = "christopherscott.ca/apps/ao3opds/feed/{feed_id}"
+FEED_TITLE = "{username}'s AO3 {feed_title} list"
 FEED_AUTHOR = OPDSPerson(
     name='Christopher Scott',
     uri='christopherscott.ca',
     email='christopher@christopherscott.ca')
+MAX_HISTORY_PAGES_DEFAULT = 3
 
-def get_AO3_session(username, password):
-    """ Authenticates a user with AO3. """
-    try:
-        session = AO3.Session(username, password)
-    except AO3.utils.LoginError as error:
-        warnings.warn(f'Could not log in to AO3 as {username}: ' + str(error))
-        return None
-    return session
-
-def marked_for_later_opds(
-        session: AO3.Session | tuple[str, str],
-        id:str=None, title:str=None, authors:list[OPDSPerson]=None,
-        threaded=False) -> str:
-    """ Returns an OPDS feed of Marked for Later lists for a user. """
-    # Log in if a (username, password) pair were provided:
-    if not isinstance(session, AO3.Session):
-        session = get_AO3_session(session[0], session[1])
-    if session is None:
-        return None
-    # Get the user's Marked for Later list:
-    works = session.get_marked_for_later()
+def _feed_opds(feed_id, works, session, id, title, authors, threaded):
+    """ Fetches a feed for `works` """
     # The current version of `ao3_api` does not set the session on works
-    # returned from `session.get_marked_for_later()`, so do that here:
+    # returned from methods such as `session.get_marked_for_later()`,
+    # so do that here:
     for work in works:
         work.set_session(session)
-
     # Default arguments:
     if id is None:
-        id=FEED_ID.format(username=session.username)
+        id=FEED_ID.format(feed_id=feed_id)
     if title is None:
-        title=FEED_TITLE.format(username=session.username)
+        title=FEED_TITLE.format(
+            username=session.username, feed_title=FEED_NAMES[feed_id])
     if authors is None:
         authors=[FEED_AUTHOR]
 
@@ -51,3 +40,57 @@ def marked_for_later_opds(
         works, id=id, title=title, authors=authors, threaded=threaded)
     feed = opds.render()
     return feed
+
+def marked_for_later_opds(
+        session: AO3.Session, id:str=None, title:str=None,
+        authors:list[OPDSPerson]=None, threaded=False) -> str:
+    """ Returns an OPDS feed of Marked for Later works for a user. """
+    if session is None:
+        return None
+    # Get the user's Marked for Later list:
+    works: list[AO3.Work] = session.get_marked_for_later()
+    feed_id = 'marked_for_later'
+    return _feed_opds(feed_id, works, session, id, title, authors, threaded)
+
+def bookmarks_opds(
+        session: AO3.Session, id:str=None, title:str=None,
+        authors:list[OPDSPerson]=None, threaded=False) -> str:
+    """ Returns an OPDS feed of bookmarks works for a user. """
+    if session is None:
+        return None
+    # Get the user's bookmarks and convert them to Works:
+    bookmarks:list[tuple] = session.get_bookmarks(use_threading=threaded)
+    works: list[AO3.Work] = [AO3.Work(
+        work_id, session, load=False) for (work_id, _, _) in bookmarks]
+    feed_id = 'bookmarks'
+    return _feed_opds(feed_id, works, session, id, title, authors, threaded)
+
+def subscriptions_opds(
+        session: AO3.Session, id:str=None, title:str=None,
+        authors:list[OPDSPerson]=None, threaded=False) -> str:
+    """ Returns an OPDS feed of works in a user's subscriptions list. """
+    if session is None:
+        return None
+    # Get the user's subscriptions (limited to Works):
+    works:list[AO3.Work] = session.get_work_subscriptions(
+        use_threading=threaded)
+    feed_id = 'subscriptions'
+    return _feed_opds(feed_id, works, session, id, title, authors, threaded)
+
+def history_opds(
+        session: AO3.Session, id:str=None, title:str=None,
+        authors:list[OPDSPerson]=None, threaded=False,
+        max_pages=MAX_HISTORY_PAGES_DEFAULT) -> str:
+    """ Returns an OPDS feed of works in a user's subscriptions list.
+
+    This method performs a first pass of a user's history in a
+    non-threaded way. The second pass, where the method loads metadata
+    for each work, supports threading.
+    """
+    if session is None:
+        return None
+    # Get the user's history (limiting pages to avoid rate-limits):
+    history:list[tuple] = session.get_history(max_pages=max_pages)
+    works:list[AO3.Work] = [work for (work, _, _) in history]
+    feed_id = 'history'
+    return _feed_opds(feed_id, works, session, id, title, authors, threaded)
